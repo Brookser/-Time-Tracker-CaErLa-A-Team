0de530443484f973a8615d5c50980c43ccdc53c4
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2025-04-17 20:11:09 -0700

    Remove extra logic to Logic/Employee.py

--- Data/Database.py
+++ Data/Database.py
@@ -19,61 +19,10 @@ class Database:
             )
 
     @classmethod
-    def get_all_employees(cls):
+    def get_cursor(cls):
         cls.connect()
-        cursor = cls.__connection.cursor()
-        cursor.execute("SELECT * FROM employee_table")
-        return cursor.fetchall()
+        return cls.__connection.cursor()
 
     @classmethod
-    def get_all_projects(cls):
-        cls.connect()
-        cursor = cls.__connection.cursor()
-        cursor.execute("SELECT * FROM projects")
-        return cursor.fetchall()
-
-    @classmethod
-    def get_departments(cls):
-        cls.connect()
-        cursor = cls.__connection.cursor()
-        cursor.execute("SELECT * FROM department")
-        return cursor.fetchall()
-
-    # Employees
-    @classmethod
-    def add_employee(cls, empid, first_name, last_name, dptid, email=None, mgr_empid=None, active=1):
-        cls.connect()
-        cursor = cls.__connection.cursor()
-        query = '''
-            INSERT INTO employee_table
-            (EMPID, FIRST_NAME, LAST_NAME, DPTID, EMAIL_ADDRESS, MGR_EMPID, EMP_ACTIVE)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
-        '''
-        cursor.execute(query, (empid, first_name, last_name, dptid, email, mgr_empid, active))
+    def commit(cls):
         cls.__connection.commit()
-
-    @classmethod
-    def get_active_employees(cls):
-        cls.connect()
-        cursor = cls.__connection.cursor()
-        cursor.execute("SELECT * FROM employee_table WHERE EMP_ACTIVE = 1")
-        return cursor.fetchall()
-
-    # @classmethod
-    # def log_time_entry(cls, employee_id, project_id, start_time, end_time, notes=""):
-    #     cls.connect()
-    #     cursor = cls.__connection.cursor()
-    #     query = '''
-    #         INSERT INTO time (employee_id, project_id, start_time, end_time, notes)
-    #         VALUES (?, ?, ?, ?, ?)
-    #     '''
-    #     cursor.execute(query, (employee_id, project_id, start_time, end_time, notes))
-    #     cls.__connection.commit()
-    #
-    # @classmethod
-    # def get_login_info(cls, username):
-    #     cls.connect()
-    #     cursor = cls.__connection.cursor()
-    #     query = "SELECT * FROM login_table WHERE username = ?"
-    #     cursor.execute(query, (username,))
-    #     return cursor.fetchone()
