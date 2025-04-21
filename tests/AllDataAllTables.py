bb6455fbc0b5f2a1b983809749350362ad9a3812
Erika Brooks <brooks.erika.a@gmail.com>
2025-04-21 16:29:49 -0700

    updated notes in AllDataAllTables

--- tests/AllDataAllTables.py
+++ tests/AllDataAllTables.py
@@ -3,7 +3,7 @@
 # Author:           Erika Brooks
 # TTfeature:        Test_01
 # Date:             04.20.2025
-# Description:      Discovers existing tables in DB, then drops all tables
+# Description:      Discovers existing tables in DB
 # Input:            none
 # Output:           confirmation message
 # Sources:          Project Charter - Jira Story: Tests 1 & 2
